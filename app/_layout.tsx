f35568f72158d96a22977ad1ecf5393b695b8e52
ryu <yongjingliu7@gmail.com>
2025-05-24 16:05:22 +0900

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Stack } from "expo-router";
+import "../global.css";
+
+const Layout = () => <Stack />;
+
+export default Layout;
---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {  useState } from 'react';
+import { Text, View, TouchableOpacity} from 'react-native';
+import WheelPickerExpo from 'react-native-wheel-picker-expo';
+import {useRouter} from 'expo-router';
+
+
+const hours = Array.from({ length: 24 }, (_, i) => i);
+const minutes = Array.from({ length: 60 }, (_, i) => i);
+const seconds = Array.from({ length: 60 }, (_, i) => i);
+
+const HomeScreen = () => {
+  const router = useRouter();
+  const [selectedHour, setSelectedHour] = useState(0);
+  const [selectedMinute, setSelectedMinute] = useState(5);
+  const [selectedSecond, setSelectedSecond] = useState(0);
+
+  return (
+    <View className="flex-1 items-center justify-start bg-black">
+      
+      <View //h m s
+      className="flex-row bg-black w-full items-center justify-center mt-9"
+      style={{ columnGap: 75 }}>
+        <Text 
+        className=' text-green-300'
+        style={{fontSize:45}}>h</Text>
+        <Text 
+        className=' text-green-300'
+        style={{fontSize:45}}>m</Text>
+        <Text 
+        className=' text-green-300'
+        style={{fontSize:45}}>s</Text>  
+      </View>
+      
+      <View //pickers 
+      className="flex-row bg-black-200 gap-10 ">
+        <WheelPickerExpo
+          height={200}
+          width={70}
+          initialSelectedIndex={0}
+          items={hours.map((num) => ({ label: num.toString(), value:num }))}
+          onChange={({ index }) => setSelectedHour(index)}
+        />
+        <WheelPickerExpo
+          height={200}
+          width={70}
+          initialSelectedIndex={5}
+          items={minutes.map((num) => ({ label: num.toString(), value:num }))}
+          onChange={({ index }) => setSelectedMinute(index)}
+        />
+        <WheelPickerExpo
+          height={200}
+          width={70}
+          initialSelectedIndex={0}
+          items={seconds.map((num) => ({ label: num.toString(), value:num }))}
+          onChange={({ index }) => setSelectedSecond(index)}
+        />
+      </View>
+
+      <TouchableOpacity // start button
+        className='bg-green-800 px-14 py-16 rounded-full mt-10'
+        onPress={() => router.push('/timerScreen')}>
+        <Text className='text-green-300'>開始</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity // setting button
+        className='bg-green-800 px-14 py-16 mt-10'>
+        <Text className='text-green-300'>設定</Text>
+      </TouchableOpacity>
+      
+
+    </View>
+  );
+};
+
+export default HomeScreen;
